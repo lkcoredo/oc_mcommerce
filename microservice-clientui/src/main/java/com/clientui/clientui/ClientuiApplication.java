44be8ce345c8b82fc720846776a7a7eead6a9727
amacabr2 <anthony.macabrey@edu.univ-fcomte.fr>
2018-09-14 17:03:07 +0200

    Création module clientui

---
+++ microservice-clientui/src/main/java/com/clientui/clientui/ClientuiApplication.java
@@ -0,0 +1,12 @@
+package com.clientui.clientui;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClientuiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClientuiApplication.class, args);
+	}
+}
