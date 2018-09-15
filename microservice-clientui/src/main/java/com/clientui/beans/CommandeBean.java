c4248c5347a790ebd096227d566a5869469a9a38
amacabr2 <anthony.macabrey@edu.univ-fcomte.fr>
2018-09-15 13:51:57 +0200

    Permet au client de passer commande

--- microservice-clientui/src/main/java/com/clientui/beans/CommandeBean.java
+++ microservice-clientui/src/main/java/com/clientui/beans/CommandeBean.java
@@ -1,4 +1,66 @@
 package com.clientui.beans;
 
+import java.util.Date;
+
 public class CommandeBean {
+
+    private int id;
+    private int productId;
+    private Date dateCommande;
+    private int quantite;
+    private boolean commandePayee;
+
+    public CommandeBean() {
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getProductId() {
+        return productId;
+    }
+
+    public void setProductId(int productId) {
+        this.productId = productId;
+    }
+
+    public Date getDateCommande() {
+        return dateCommande;
+    }
+
+    public void setDateCommande(Date dateCommande) {
+        this.dateCommande = dateCommande;
+    }
+
+    public int getQuantite() {
+        return quantite;
+    }
+
+    public void setQuantite(int quantite) {
+        this.quantite = quantite;
+    }
+
+    public boolean isCommandePayee() {
+        return commandePayee;
+    }
+
+    public void setCommandePayee(boolean commandePayee) {
+        this.commandePayee = commandePayee;
+    }
+
+    @Override
+    public String toString() {
+        return "CommandeBean{" +
+                "id=" + id +
+                ", productId=" + productId +
+                ", dateCommande=" + dateCommande +
+                ", quantite=" + quantite +
+                ", commandePayee=" + commandePayee +
+                '}';
+    }
 }
--- microservice-clientui/src/main/java/com/clientui/controller/ClientController.java
+++ microservice-clientui/src/main/java/com/clientui/controller/ClientController.java
@@ -1,6 +1,8 @@
 package com.clientui.controller;
 
+import com.clientui.beans.CommandeBean;
 import com.clientui.beans.ProductBean;
+import com.clientui.proxies.MicroserviceCommandesProxy;
 import com.clientui.proxies.MicroserviceProduitsProxy;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -8,6 +10,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.util.Date;
 import java.util.List;
 
 @Controller
@@ -15,6 +18,9 @@ public class ClientController {
 
     @Autowired
     private MicroserviceProduitsProxy produitsProxy;
+
+    @Autowired
+    private MicroserviceCommandesProxy commandesProxy;
 
     @RequestMapping("/")
     public String accueil(Model model) {
@@ -28,5 +34,21 @@ public class ClientController {
         ProductBean produit = produitsProxy.recupererUnProduit(id);
         model.addAttribute("produit", produit);
         return "ficheProduit";
+    }
+
+    @RequestMapping(value = "/commander-produit/{idProduit}/{montant}")
+    public String passerCommande(@PathVariable int idProduit, @PathVariable Double montant,  Model model){
+        CommandeBean commande = new CommandeBean();
+
+        commande.setProductId(idProduit);
+        commande.setQuantite(1);
+        commande.setDateCommande(new Date());
+
+        CommandeBean commandeAjoutee = commandesProxy.ajouterCommande(commande);
+
+        model.addAttribute("commande", commandeAjoutee);
+        model.addAttribute("montant", montant);
+
+        return "paiement";
     }
 }
