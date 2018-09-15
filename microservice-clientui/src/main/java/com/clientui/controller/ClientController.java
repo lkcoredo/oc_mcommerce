0d49294080c8651a8b9b8329a4098e6f50747515
amacabr2 <anthony.macabrey@edu.univ-fcomte.fr>
2018-09-15 12:10:24 +0200

    Récupération des détails d'un produit

--- microservice-clientui/src/main/java/com/clientui/controller/ClientController.java
+++ microservice-clientui/src/main/java/com/clientui/controller/ClientController.java
@@ -5,6 +5,7 @@ import com.clientui.proxies.MicroserviceProduitsProxy;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.List;
@@ -20,5 +21,12 @@ public class ClientController {
         List<ProductBean> produits = produitsProxy.listeDesProduits();
         model.addAttribute("produits", produits);
         return "accueil";
+    }
+
+    @RequestMapping("/details-produit/{id}")
+    public String ficheProduit(@PathVariable int id, Model model){
+        ProductBean produit = produitsProxy.recupererUnProduit(id);
+        model.addAttribute("produit", produit);
+        return "ficheProduit";
     }
 }
